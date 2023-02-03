48a40c6063b3316ed41e5c93f639125ad959a1de
wbhnn <949988739@qq.com>
2023-02-03 09:54:08 +0800

    spring-boot 集成原生的`swagger` 用于统一管理、测试 API 接口

---
+++ practice-swagger/src/main/java/com/practice/PracticeSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class PracticeSwaggerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ practice-swagger/src/main/java/com/practice/swagger/common/ParamType.java
@@ -0,0 +1,11 @@
+package com.practice.swagger.common;
+
+public final class ParamType {
+
+    public final static String QUERY = "query";
+    public final static String HEADER = "header";
+    public final static String PATH = "path";
+    public final static String BODY = "body";
+    public final static String FORM = "form";
+
+}
