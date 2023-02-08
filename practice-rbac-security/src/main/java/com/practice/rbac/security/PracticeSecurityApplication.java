fa655b5b77611f383b6cc0eb29a8556f7d7be815
wbhnn <949988739@qq.com>
2023-02-08 14:37:31 +0800

    spring-boot 集成spring security

---
+++ practice-rbac-security/src/main/java/com/practice/rbac/security/PracticeSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.practice.rbac.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PracticeSecurityApplication!
+ * @author wb
+ */
+@SpringBootApplication
+public class PracticeSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSecurityApplication.class,args);
+    }
+}
