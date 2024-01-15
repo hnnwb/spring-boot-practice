77b2eab006f08496107ccae240199782c6a87b40
wbhnn <949988739@qq.com>
2024-01-15 15:39:48 +0800

    spring-boot 集成第三方登录，继承justauth-spring-boot-starter 实现QQ登录，github登录，微信登录，谷歌登录等等

---
+++ practice-social/src/main/java/com/practice/SocialApplication.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author wb
+ */
+@SpringBootApplication
+public class SocialApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+}
