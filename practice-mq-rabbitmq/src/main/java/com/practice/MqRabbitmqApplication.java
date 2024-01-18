c64732b10bfc7f503a425b2eb072e7097886502c
wbhnn <949988739@qq.com>
2024-01-18 13:55:36 +0800

    spring-boot 集成 RabbitMQ 实现基于直接队列模式、分列模式、主题模式、延迟队列的消息发送和接收

---
+++ practice-mq-rabbitmq/src/main/java/com/practice/MqRabbitmqApplication.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author wb
+ */
+@SpringBootApplication
+public class MqRabbitmqApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MqRabbitmqApplication.class, args);
+    }
+}
---
+++ practice-mq-rabbitmq/src/main/java/com/practice/rabbitmq/message/MessageStruct.java
@@ -0,0 +1,22 @@
+package com.practice.rabbitmq.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 测试消息体
+ * @author wb
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageStruct implements Serializable {
+    private static final long serialVersionUID = 392365881428311040L;
+
+    private String message;
+}
