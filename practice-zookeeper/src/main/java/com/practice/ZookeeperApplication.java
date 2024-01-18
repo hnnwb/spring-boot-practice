64032f8d546a77a5652a1cd05cb12101e95c05f2
wbhnn <949988739@qq.com>
2024-01-18 13:16:27 +0800

    spring-boot 集成 Zookeeper 结合AOP实现分布式锁

---
+++ practice-zookeeper/src/main/java/com/practice/ZookeeperApplication.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author wb
+ */
+@SpringBootApplication
+public class ZookeeperApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperApplication.class, args);
+    }
+}
