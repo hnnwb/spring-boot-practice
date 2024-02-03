8f333ff0c6dad61bbfaa0429d3760575635e72ce
wbhnn <949988739@qq.com>
2024-02-03 13:09:36 +0800

    spring-boot 集成 ureport2 实现复杂的自定义的中国式报表

---
+++ practice-ureport2/src/main/java/com/practice/Ureport2Application.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ * @author wb
+ */
+@SpringBootApplication
+public class Ureport2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ureport2Application.class, args);
+    }
+}
---
+++ practice-ureport2/src/main/java/com/practice/ureport2/config/InnerDatasource.java
@@ -0,0 +1,26 @@
+package com.practice.ureport2.config;
+
+import com.bstek.ureport.definition.datasource.BuildinDatasource;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+public class InnerDatasource implements BuildinDatasource {
+    @Autowired
+    private DataSource datasource;
+
+    @Override
+    public String name() {
+        return "内部数据源";
+    }
+
+    @SneakyThrows
+    @Override
+    public Connection getConnection() {
+        return datasource.getConnection();
+    }
+}
