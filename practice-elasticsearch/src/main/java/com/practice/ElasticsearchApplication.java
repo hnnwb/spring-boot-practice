0fe92ea4c5409024a065c7c49e4d588f8b8647c1
wbhnn <949988739@qq.com>
2024-02-04 09:44:44 +0800

    spring-boot 集成 ElasticSearch，集成`spring-boot-starter-data-elasticsearch` 完成对 ElasticSearch 的高级使用技巧，包括创建索引、配置映射、删除索引、增删改查基本操作、复杂查询、高级查询、聚合查询

---
+++ practice-elasticsearch/src/main/java/com/practice/ElasticsearchApplication.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author wb
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+}
---
+++ practice-elasticsearch/src/main/java/com/practice/elasticsearch/constants/EsConsts.java
@@ -0,0 +1,13 @@
+package com.practice.elasticsearch.constants;
+
+public interface EsConsts {
+    /**
+     * 索引名称
+     */
+    String INDEX_NAME = "person";
+
+    /**
+     * 类型名称
+     */
+    String TYPE_NAME = "person";
+}
