9af4ff367155557c06929fcd290d50033045ff1c
wbhnn <949988739@qq.com>
2024-03-11 13:08:33 +0800

    springboot用docker容器化部署

---
+++ practice-docker/src/main/java/com/practice/PracticeDockerApplication.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author wb
+ */
+@SpringBootApplication
+public class PracticeDockerApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeDockerApplication.class, args);
+    }
+}
