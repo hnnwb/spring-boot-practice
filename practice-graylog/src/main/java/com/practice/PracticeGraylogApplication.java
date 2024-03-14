f0fff54097ea71cc5dc1b0b22ce74e3ce7cdb874
wbhnn <949988739@qq.com>
2024-03-14 13:13:25 +0800

    spring-boot 集成 graylog 实现日志统一收集

---
+++ practice-graylog/src/main/java/com/practice/PracticeGraylogApplication.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ * @author wb
+ */
+@SpringBootApplication
+public class PracticeGraylogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeGraylogApplication.class, args);
+    }
+}
